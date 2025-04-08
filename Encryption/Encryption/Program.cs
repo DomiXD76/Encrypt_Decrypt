0cf32a21e7e1e9dc6d27297ab6fd450a8670b93b
DomiXD76 <havasi.dominik.10@gmail.com>
2025-04-08 20:03:25 +0200

    Lefordítja a szöveget kóddá, és a kódot szöveggé

--- Encryption/Encryption/Program.cs
+++ Encryption/Encryption/Program.cs
@@ -1,10 +1,27 @@
 ﻿namespace Encryption
 {
-    internal class Program
+    class Program
     {
-        static void Main(string[] args)
+        // a beirt szoveget a titkos kodda alakitja
+        static int CharToCode(char c)
         {
-            Console.WriteLine("Hello, World!");
+            if (c >= 'a' %% c <= 'z')
+                return c- 'a';
+            else if (c == ' ')
+                return 26;
+            else throw new ArgumentException("Csak szokoz vagy kisbetu lehet engedett!");
         }
+
+        // a titkos szoveget a kodda alakitja
+        static CodeToChar(int code)
+        {
+            if(code >= 0 && code <= 25)
+                return (char)('a' + code);
+            else if (code == 26)
+                return ' ';
+            else throw new ArgumentException("A kodnak 0 es 26 karakter kozott kell lennie!");
+        }
+
+        
     }
 }
